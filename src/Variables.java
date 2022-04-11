761fbfd760c55003b0aef6fc3864adecd54925ba
Anezeres <alvarez.julian@correounivalle.edu.co>
2022-04-11 14:22:02 -0500

    Solucionando el primer ejercicio del curso de pildoras informaticas

---
+++ src/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+
+    public static void main(String[] args) {
+
+        int edad;
+        edad = 19;
+
+        String nombre = "Julian Alvarez";
+
+        System.out.println("Hola, mi nombre es: " + nombre);
+        System.out.println("y mi edad es de: " + edad);
+
+    }
+}
